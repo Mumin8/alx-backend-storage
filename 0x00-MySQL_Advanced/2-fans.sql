675fad76430ac7ef4edf25b60e5f6bbc1caca691
Mumin8 <alhassanmumin8@gmail.com>
2024-03-21 22:04:24 +0000

    uniqe user

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,8 +1,9 @@
 -- 3 first students in the Batch ID=3
 -- because Batch 3 is the best!
 
-SELECT origin, SUM(nb_fans) AS total_fans
+SELECT origin, SUM(nb_fans) as nb_fans
 FROM metal_bands
 GROUP BY origin
-ORDER BY total_fans DESC;
+ORDER BY nb_fans DESC;
+
 
