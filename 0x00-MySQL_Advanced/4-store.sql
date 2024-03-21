fa42a94dcdd7bbeaefdd000a9227aecf33f5a669
Mumin8 <alhassanmumin8@gmail.com>
2024-03-21 22:09:56 +0000

    mysql advance

---
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -0,0 +1,9 @@
+-- 3 first students in the Batch ID=3
+-- because Batch 3 is the best!
+
+CREATE TRIGGER decrease_quantity_after_order
+AFTER INSERT ON orders
+FOR EACH ROW
+UPDATE items
+SET quantity = quantity - NEW.number
+WHERE name = NEW.item_name;
