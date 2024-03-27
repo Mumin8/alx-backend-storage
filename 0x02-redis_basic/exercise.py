daa139d0d3f6308a5b992d5502dc3a3347fa8891
Mumin8 <alhassanmumin8@gmail.com>
2024-03-27 23:07:00 +0000

    redis

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -4,7 +4,7 @@
 
 import uuid
 import redis
-from typing import Union
+from typing import Union, Callable
 
 class Cache:
     '''
@@ -24,3 +24,27 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Callable = None) -> Union[str, bytes, int, float]:
+        '''
+        a method that returns the key
+        '''
+        data = self._redis.get(key)
+        if data is None:
+            return None
+        if fn is not None:
+            return fn(data)
+        return data
+
+    def get_str(self, key: str) -> str:
+        '''
+        a method that gets the string
+        '''
+        return self.get(key, fn=lambda d: d.decode('utf-8'))
+
+    def get_int(self, key: str) -> int:
+        '''
+        a method that gets the int
+        '''
+        return self.get(key, fn=lambda d: int(d))
+        
